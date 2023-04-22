01375a200ae1602925a4eaf3ac6dff883c8e3009
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 13:23:14 +0200

    added app icon

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-const { log } = require('console');
-const fs = require('fs');
+// const { log } = require('console');
+// const fs = require('fs');
 
 const data = process.env.BODY;
 
-const lines = data.split("\n");
+console.log("====================>", data);
 
-const name = lines[2];
-const domainName = lines[6];
-const documentationUrl = lines[10] !== "_No response_" ? lines[10] : null;
-const supported2FAMethods = lines
-  .slice(14)
-  .filter(line => line.startsWith("- [X]"))
-  .map(line => {
-    const method = line.substring(5).trim();
-    if (method === "TOTP (Google Authenticator)") {
-      return "totp";
-    } else {
-      return method.toLowerCase();
-    }
-  });
+// const lines = data.split("\n");
 
-const json = {
-  [name]: {
-    domain: domainName,
-    ...(documentationUrl && {documentation: documentationUrl}),
-    tfa: supported2FAMethods
-  }
-};
+// const name = lines[2];
+// const domainName = lines[6];
+// const documentationUrl = lines[10] !== "_No response_" ? lines[10] : null;
+// const supported2FAMethods = lines
+//   .slice(14)
+//   .filter(line => line.startsWith("- [X]"))
+//   .map(line => {
+//     const method = line.substring(5).trim();
+//     if (method === "TOTP (Google Authenticator)") {
+//       return "totp";
+//     } else {
+//       return method.toLowerCase();
+//     }
+//   });
 
-console.log(json)
+// const json = {
+//   [name]: {
+//     domain: domainName,
+//     ...(documentationUrl && {documentation: documentationUrl}),
+//     tfa: supported2FAMethods
+//   }
+// };
 
-fs.writeFileSync(domainName + '.json', JSON.stringify(json, null, 2));
+// console.log(json)
+
+// fs.writeFileSync(domainName + '.json', JSON.stringify(json, null, 2));
