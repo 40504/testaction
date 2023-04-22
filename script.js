953355a1fbdfb138d1124e79def3322793b3a529
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 17:39:49 +0200

    update

--- script.js
+++ script.js
@@ -4,32 +4,32 @@ const fs = require('fs');
 const data = process.env.BODY.substring(1, process.env.BODY.length - 1);
 console.log("Body1:", data);
 
-const lines = data.split("\n\n");
+const lines = data.split("\r\n");
 console.log("lines:", lines);
 
-const name = lines[4];
-const domainName = lines[7];
-const documentationUrl = lines[10] !== "_No response_" ? lines[10] : null;
-const supported2FAMethods = lines
-  .slice(14)
-  .filter(line => line.startsWith("- [X]"))
-  .map(line => {
-    const method = line.substring(5).trim();
-    if (method === "TOTP (Google Authenticator)") {
-      return "totp";
-    } else {
-      return method.toLowerCase();
-    }
-  });
+// const name = lines[4];
+// const domainName = lines[7];
+// const documentationUrl = lines[10] !== "_No response_" ? lines[10] : null;
+// const supported2FAMethods = lines
+//   .slice(14)
+//   .filter(line => line.startsWith("- [X]"))
+//   .map(line => {
+//     const method = line.substring(5).trim();
+//     if (method === "TOTP (Google Authenticator)") {
+//       return "totp";
+//     } else {
+//       return method.toLowerCase();
+//     }
+//   });
 
-const json = {
-  [name]: {
-    domain: domainName,
-    ...(documentationUrl && {documentation: documentationUrl}),
-    tfa: supported2FAMethods
-  }
-};
+// const json = {
+//   [name]: {
+//     domain: domainName,
+//     ...(documentationUrl && {documentation: documentationUrl}),
+//     tfa: supported2FAMethods
+//   }
+// };
 
-console.log("***************", json)
+// console.log("***************", json)
 
-fs.writeFileSync(domainName + '.json', JSON.stringify(json, null, 2));
+// fs.writeFileSync(domainName + '.json', JSON.stringify(json, null, 2));
