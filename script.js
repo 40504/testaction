b8efabf9526221055c750832e07e9ee433548e8f
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 17:41:39 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // const { log } = require('console');
 const fs = require('fs');
 
-const data = process.env.BODY.substring(1, process.env.BODY.length - 1);
+const data = process.env.BODY.substring(1, process.env.BODY.length - 1).replace("\n", "");
 console.log("Body1:", data);
 
 const lines = data.split("\r\n");
