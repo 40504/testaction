b3f5e14ea2d8291b2b9587270c4ea62a45b0339f
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 17:16:17 +0200

    update

--- script.js
+++ script.js
@@ -4,19 +4,13 @@ const fs = require('fs');
 const data = process.env.BODY;
 console.log("Body:", data);
 
+const data2 = "### Name\n\nDropbox\n\n### Domain\n\ndropbox.com\n\n### Documentation\n\nhttps://help.dropbox.com/account-access/enable-two-step-verification\n\n### Supported 2FA Methods\n\n- [X] TOTP (Google Authenticator)\n- [ ] U2F\n- [ ] sms\n- [ ] email";
+console.log("Body:", data2);
+
 const lines = data.split("\r\n");
 console.log("lines:", lines);
 
-
-const nameMatch = data[0].match(/### Name\n\n(.+)/);
-console.log("nameMatch:", nameMatch);
-const nameline = nameMatch ? nameMatch[1] : null;
-console.log("nameline:", nameline);
-
-const name = lines[1].match(/### Name\n\n(.+)/);
-const namel = name ? name[1] : null;
-console.log("name:", namel);
-
+const name = lines[4];
 const domainName = lines[7];
 const documentationUrl = lines[10] !== "_No response_" ? lines[10] : null;
 const supported2FAMethods = lines
