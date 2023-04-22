ad8a57aa50cc5ed174c40663b7de8f3b5dc13959
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 14:28:42 +0200

    update

--- parse2.js
+++ parse2.js
@@ -2,16 +2,17 @@ const data = process.env.BODY;
 
 // Split the text into an array of lines
 const lines = data.split('\n');
-
+console.log("lines======>", lines)
 // Initialize an empty object to store the parsed data
 const parsedData = {};
 
 // Loop through the lines and extract the relevant information
 for (let i = 0; i < lines.length; i++) {
   const line = lines[i].trim();
-  
+  console.log("lines======>", line)
   if (line.startsWith('### Name')) {
     parsedData.name = lines[i+1].trim();
+    console.log("lines======>", parsedData.name)
   }
   else if (line.startsWith('### Domain')) {
     parsedData.domain = lines[i+1].trim();
