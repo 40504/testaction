53d4f9ebda42d542899c7ecd3f4d412f4b7865b8
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 16:22:57 +0200

    update

--- script.js
+++ script.js
@@ -4,16 +4,10 @@ const fs = require('fs');
 const data = process.env.BODY;
 console.log("Body:", data);
 
-const lines = data.split("\n");
+const lines = data.split("\r\n");
 console.log("lines:", lines);
 
-const name = lines[4];
-console.log("name:", name);
-const name1 = lines[1];
-console.log("name1:", name1);
-const name0 = lines[0];
-console.log("name0:", name0);
-
+const name = lines[3];
 const domainName = lines[7];
 const documentationUrl = lines[10] !== "_No response_" ? lines[10] : null;
 const supported2FAMethods = lines
