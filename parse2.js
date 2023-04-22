38c4d2ebc877ebe1e5cb35aebeb3fde76455a860
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-22 16:06:09 +0200

    update

--- parse2.js
+++ parse2.js
@@ -13,7 +13,7 @@ for (let i = 0; i < lines.length; i++) {
   console.log("lines======>", line)
   if (line.startsWith('### Name')) {
     parsedData.name = lines[i+1].trim();
-    console.log("lines======>", parsedData)
+    console.log("lines======>", parsedData.name)
   }
   else if (line.startsWith('### Domain')) {
     parsedData.domain = lines[i+1].trim();
