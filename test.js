38bc7555eccb03dcded045bca166c70a42182ecb
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-23 06:41:46 +0200

    test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fileName = 'myFile.txt';
 console.log(fileName);
 const fileContent = process.env.BODY;
-const lines = fileContent.split("\r\n");
+const lines = fileContent.split("\n");
 console.log(lines);
 
 fs.writeFileSync(fileName, lines, (err) => {
