f7fa96fba84f5ac6a3fe052934926175318751c3
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-23 06:31:26 +0200

    test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const fileName = 'myFile.txt';
 console.log(fileName);
-const fileContent = process.env.BODY;
+const fileContent = "process.env.BODY";
 
 fs.writeFileSync(fileName, fileContent, (err) => {
   if (err) throw err;
