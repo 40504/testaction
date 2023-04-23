a47b6b399cb44a55a39460aa188fcb7a0732b446
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-23 06:43:36 +0200

    test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const fileName = 'myFile.txt';
 console.log(fileName);
-const fileContent = process.env.BODY;
+const fileContent = process.env.BODY.substring(1, process.env.BODY.length - 1).replace("\n", "");
 const lines = fileContent.split("\n");
 console.log(lines);
 
