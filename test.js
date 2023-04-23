bcef301f5136eeb41887099e07210bf194e64e70
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-23 06:54:14 +0200

    test

--- test.js
+++ test.js
@@ -4,8 +4,10 @@ console.log(fileName);
 const fileContent = process.env.BODY;
 typeof fileContent === "string";
 console.log("fileContent:", fileContent);
-
 console.log(typeof fileContent);
+
+const lines = fileContent.split("\n");
+console.log("lines:", lines);
 
 fs.writeFileSync(fileName, fileContent, (err) => {
   if (err) throw err;
