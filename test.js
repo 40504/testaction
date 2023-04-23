246f47a47b25c08b80ec3bf660c8b8ce9760f90b
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-23 06:39:58 +0200

    test

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const fileName = 'myFile.txt';
 console.log(fileName);
 const fileContent = process.env.BODY;
+const lines = fileContent.split("\r\n");
+console.log(lines);
 
-fs.writeFileSync(fileName, fileContent, (err) => {
+fs.writeFileSync(fileName, lines, (err) => {
   if (err) throw err;
   console.log('File created successfully');
 });
