e2136e84a4248230a1cf1366fc6b3668dd45fde8
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-04-25 00:17:32 +0200

    test

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ console.log("lines:", lines);
 const name = lines[1];
 const domainName = lines[3];
 const documentationUrl = lines[5] !== "_No response_" ? lines[5] : null;
-const tfalines = lines[6];
-console.log("lines:", tfalines);
+const tfalines = lines[7];
+console.log("tfalines:", tfalines);
 const falines = tfalines.split("\\n");
 console.log("falines:", falines);
 
