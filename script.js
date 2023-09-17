f7eb99016b2eff86c94d87bb06910a6dd8b8912a
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-17 03:21:57 +0200

    update

--- script.js
+++ script.js
@@ -33,4 +33,4 @@ fs.writeFileSync(filePath, JSON.stringify(json, null, 2), err => {
 // Your JavaScript code
 const myValue = "Hello, GitHub Actions!";
 console.log("from .js file:", myValue);
-console.log(`::set-env name=MY_VALUE::${myValue}`);
+fs.writeFileSync('myValue.txt', myValue, 'utf-8');
