f33095b9e9cf81707fa317dc88492213df60f540
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-17 02:07:26 +0200

    update

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const lines = data.split("\\n\\n");
 const name = lines[1];
 const domainName = lines[3];
 const documentationUrl = lines[5] !== "_No response_" ? lines[5] : null;
-const supported_2FA_methods = lines[7].split("\\n");
+const recoveryUrl = lines[7] !== "_No response_" ? lines[7] : null;
+const supported_2FA_methods = lines[9].split("\\n");
 const tfa_lines = supported_2FA_methods
     .filter(line => line.startsWith('- [X]'))
     .map(line => line.substring(6));
@@ -16,18 +17,15 @@ const json = {
   [name]: {
     domain: domainName,
     ...(documentationUrl && {documentation: documentationUrl}),
+    ...(recoveryUrl && {recovery: recoveryUrl}),
     tfa: tfa_lines
   }
 };
 
 console.log("***************", json)
 
-const filePath = path.join(__dirname, 'files', domainName + '.json');
+const filePath = path.join(__dirname, '/entries/zauthenticator/', domainName + '.json');
 fs.writeFileSync(filePath, JSON.stringify(json, null, 2), err => {
     if (err) throw err;
     console.log('Saved the filtered lines to', filePath);
   });
-
-module.exports = ({github, context}) => {
-  return context.payload.client_payload.data
-}
