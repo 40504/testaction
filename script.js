836d33459b54e1f23639c00fcb46fce3b354df32
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-17 03:09:15 +0200

    update

--- script.js
+++ script.js
@@ -33,3 +33,4 @@ fs.writeFileSync(filePath, JSON.stringify(json, null, 2), err => {
 // Your JavaScript code
 const myValue = "Hello, GitHub Actions!";
 console.log("from .js file:", myValue);
+console.log(`::set-output name=myValue::${myValue}`);
