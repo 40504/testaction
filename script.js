f70742394494fae971dee97d2dde5d78fb831883
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-19 01:17:17 +0200

    update

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-const axios = require('axios');
-
-const githubToken = process.env.GITHUB_TOKEN;
-const owner = process.env.GITHUB_USERNAME;
-const repo = process.env.GITHUB_REPOSITORY;
-const issueNumber = 385; // Replace with the actual issue number
-const commentBody = 'This is a comment from a GitHub Actions workflow.';
-
-axios.post(`https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/comments`, {
-  body: commentBody
-}, {
-  headers: {
-    Authorization: `Bearer ${githubToken}`,
-    'Content-Type': 'application/json'
-  }
-})
-.then(response => {
-  console.log('Comment created successfully:', response.data);
-})
-.catch(error => {
-  console.error('Error creating comment:', error);
-});
+const myValue = "Hello, GitHub Actions!";
+console.log(`::set-output name=myValue::${myValue}`);
