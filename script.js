6fa76aea77fc55b444cc6d437ebb5e53771fb43f
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-18 02:39:48 +0200

    update

--- script.js
+++ script.js
@@ -35,4 +35,6 @@ fs.writeFileSync(filePath, JSON.stringify(json, null, 2), err => {
 // console.log("domain name from js - ", myValue);
 // fs.writeFileSync(myValue + '.txt', myValue, 'utf-8');
 
+return domainName;
+
 const myValue = domainName;
