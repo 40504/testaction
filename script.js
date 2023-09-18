13f6f5a756112a5bd37aa97ff61480895bede2c9
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-19 01:07:55 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const githubToken = process.env.GITHUB_TOKEN;
 const owner = process.env.GITHUB_USERNAME;
 const repo = process.env.GITHUB_REPOSITORY;
-const issueNumber = 1; // Replace with the actual issue number
+const issueNumber = 385; // Replace with the actual issue number
 const commentBody = 'This is a comment from a GitHub Actions workflow.';
 
 axios.post(`https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/comments`, {
---
+++ scripts.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = process.env.BODY.replaceAll('"', "");
+const lines = data.split("\\n\\n");
+
+const name = lines[1];
+const domainName = lines[3];
+const documentationUrl = lines[5] !== "_No response_" ? lines[5] : null;
+const recoveryUrl = lines[7] !== "_No response_" ? lines[7] : null;
+const supported_2FA_methods = lines[9].split("\\n");
+const tfa_lines = supported_2FA_methods
+    .filter(line => line.startsWith('- [X]'))
+    .map(line => line.substring(6));
+
+const json = {
+  [name]: {
+    domain: domainName,
+    ...(documentationUrl && {documentation: documentationUrl}),
+    ...(recoveryUrl && {recovery: recoveryUrl}),
+    tfa: tfa_lines
+  }
+};
+
+console.log("***************\n", json)
+
+const filePath = path.join(__dirname, 'files', domainName + '.json');
+fs.writeFileSync(filePath, JSON.stringify(json, null, 2), err => {
+    if (err) throw err;
+    console.log('Saved the filtered lines to', filePath);
+  });
+  
+// Your JavaScript code
+// const myValue = domainName;
+// console.log("domain name from js - ", myValue);
+// fs.writeFileSync(myValue + '.txt', myValue, 'utf-8');
+
+const myValue = domainName;
