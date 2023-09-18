9cfd81254b2ae04c44be3a65926f169e653b62d2
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-19 01:43:08 +0200

    update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+const fs = require('fs');
+
 const myValue = "Hello, GitHub Actions!";
-console.log(`"{name}=${myValue}" >> $GITHUB_OUTPUT`);
+// console.log(`"{name}=${myValue}" >> $GITHUB_OUTPUT`);
+
+const name = "myVariableName";
+const value = "myVariableValue";
+
+fs.writeFileSync(process.env.GITHUB_OUTPUT, `${name}=${value}\n`, { flag: 'a' });
