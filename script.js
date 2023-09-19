7133acae4882f4109aeeb33a1dc17d2f27048714
Aries Ikel <71176569+40504@users.noreply.github.com>
2023-09-19 02:14:18 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
 const myValue = "Hello, GitHub Actions!";
-const fs = require('fs');
-
-fs.writeFileSync('.env', `MY_VALUE=${myValue}`);
-console.log(`Environment file created with MY_VALUE=${myValue}`);
+console.log(`::set-output name=MY_VALUE::${myValue}`);
